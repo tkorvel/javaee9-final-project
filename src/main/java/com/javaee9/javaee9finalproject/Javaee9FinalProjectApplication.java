1c9feba5142945c32569af9d41d0b0cc79edfe89
tkorvel <korveltanel@gmail.com>
2022-03-19 11:03:18 +0200

    [init] - project skeleton created

---
+++ src/main/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javaee9.javaee9finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaee9FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaee9FinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaee9/javaee9finalproject/Javaee9FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee9.javaee9finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaee9FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
